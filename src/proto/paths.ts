188eea68b9eec7a0ec1e6a0a0a733e67b59adc47
bagdenius <bagdensparrow@gmail.com>
2026-03-16 15:48:30 +0200

    feat: add user proto with user message, get me and create user rpc methods

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -3,4 +3,5 @@ import { join } from 'path';
 export const PROTO_PATHS = {
   AUTH: join(__dirname, '../../proto/auth.proto'),
   ACCOUNT: join(__dirname, '../../proto/account.proto'),
+  USER: join(__dirname, '../../proto/user.proto'),
 } as const;
