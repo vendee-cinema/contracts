2f2ee932261edd6597e072228bf211855f5bf92c
bagdenius <bagdensparrow@gmail.com>
2026-03-20 23:42:27 +0200

    feat: add media package to proto paths

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -4,4 +4,5 @@ export const PROTO_PATHS = {
   AUTH: join(__dirname, '../../proto/auth.proto'),
   ACCOUNT: join(__dirname, '../../proto/account.proto'),
   USER: join(__dirname, '../../proto/user.proto'),
+  MEDIA: join(__dirname, '../../proto/media.proto'),
 } as const;
