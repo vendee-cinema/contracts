36f4f1632b0be64a279d78f4776a4c027a6dbadc
bagdenius <bagdensparrow@gmail.com>
2026-03-23 17:46:20 +0200

    feat: unify src and generated TS in flat dist + namespace exports
    - Add namespace exports for subpackages (account, auth, movie, etc.)
    - Compile both src and gen/ts directly into flat dist folder
    - Update tsconfig.build.json to include src and generated TS
    - Add prepare script to run generate + build automatically
    - Setup package.json exports for subpath imports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 export * from './proto';
 export * from './events';
+
+export * as account from '../gen/ts/account';
+export * as auth from '../gen/ts/auth';
+export * as category from '../gen/ts/category';
+export * as media from '../gen/ts/media';
+export * as movie from '../gen/ts/movie';
+export * as theater from '../gen/ts/theater';
+export * as user from '../gen/ts/user';
